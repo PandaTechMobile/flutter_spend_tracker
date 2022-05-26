a09105ee746a7ef582e3e5a8af86514eb77d24de
CarlosMobileAppDeveloper <carl.d.ormerod@gmail.com>
2022-05-27 07:49:42 +1000

    finished 27 and fixed bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+      ),
       home: MyHomePage(),
     );
   }
@@ -80,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Expense Tracker'),
         actions: [
           IconButton(
             onPressed: () => _showAddNewTransaction(context),
--- lib/widgets/new_transaction_widget.dart
+++ lib/widgets/new_transaction_widget.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class NewTransaction extends StatefulWidget {
   final Function addTransaction;
 
   NewTransaction(this.addTransaction);
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void _submitAddTransaction() {
     final enteredTitle = titleController.text;
@@ -13,7 +20,10 @@ class NewTransaction extends StatelessWidget {
 
     if (enteredTitle.isEmpty || enteredAmount <= 0) return;
 
-    addTransaction(enteredTitle, enteredAmount);
+    widget.addTransaction(enteredTitle, enteredAmount);
+
+    // NOTE / TODO -> We can send the fricking result back! So no need to pass in the bloody method...
+    Navigator.of(context).pop();
   }
 
   @override
