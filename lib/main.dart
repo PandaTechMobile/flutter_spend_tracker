ed07103ffdc85d002ae95a4e3c045e66e0fbecd9
PandaTechMobile <103789129+PandaTechMobile@users.noreply.github.com>
2022-06-01 08:38:41 +1000

    Continued responsive changes (Ep4Chp13) - small cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -174,8 +174,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
     final appBar = AppBar(
       title: const Text(
         'Expense Tracker',
@@ -189,9 +189,9 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     final txnListWidget = Container(
-      height: (MediaQuery.of(context).size.height -
+      height: (mediaQuery.size.height -
               appBar.preferredSize.height -
-              MediaQuery.of(context).padding.top) *
+              mediaQuery.padding.top) *
           0.75,
       child: TransactionList(_userTransactions, _deleteTransaction),
     );
@@ -230,9 +230,9 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             if (!isLandscape)
               Container(
-                height: (MediaQuery.of(context).size.height -
+                height: (mediaQuery.size.height -
                         appBar.preferredSize.height -
-                        MediaQuery.of(context).padding.top) *
+                        mediaQuery.padding.top) *
                     0.25,
                 child: Chart(_recentTransactions),
               ),
@@ -240,9 +240,9 @@ class _MyHomePageState extends State<MyHomePage> {
             if (isLandscape)
               _showChart
                   ? Container(
-                      height: (MediaQuery.of(context).size.height -
+                      height: (mediaQuery.size.height -
                               appBar.preferredSize.height -
-                              MediaQuery.of(context).padding.top) *
+                              mediaQuery.padding.top) *
                           0.75,
                       child: Chart(_recentTransactions),
                     )
